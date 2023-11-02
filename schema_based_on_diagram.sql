e20695250d58945e7cf8ba6e1d8dfa5add83aa27
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-02 10:25:15 +0300

    CREATE TABLE "invoice_items"

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "invoice_items"
+(
+  "id" SERIAL PRIMARY KEY,
+  "unit_price" DECIMAL,
+  "quantity" INTEGER,
+  "total_price" DECIMAL,
+  "invoice_id" INTEGER,
+  "treatment_id" INTEGER,
+  FOREIGN KEY ("invoice_id") REFERENCES "invoices" ("id"),
+  FOREIGN KEY ("treatment_id") REFERENCES "treatments" ("id")
+);
+
+CREATE INDEX ON "invoice_items"
+("invoice_id");
+CREATE INDEX ON "invoice_items"
+("treatment_id");
