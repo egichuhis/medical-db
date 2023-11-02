d7047269e03137da07470fb7327a81dca3f5634c
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-11-02 08:47:17 +0000

    CREATE INDEX ON invoice_item on medical-db

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,23 @@ CREATE TABLE "invoices"
   "medical_history_id" INTEGER,
   FOREIGN KEY ("medical_history_id") REFERENCES "medical_histories" ("id")
 );
+
+CREATE INDEX ON "invoices"
+("medical_history_id");
+
+CREATE TABLE "invoice_items"
+(
+  "id" SERIAL PRIMARY KEY,
+  "unit_price" DECIMAL,
+  "quantity" INTEGER,
+  "total_price" DECIMAL,
+  "invoice_id" INTEGER,
+  "treatment_id" INTEGER,
+  FOREIGN KEY ("invoice_id") REFERENCES "invoices" ("id"),
+  FOREIGN KEY ("treatment_id") REFERENCES "treatments" ("id")
+);
+
+CREATE INDEX ON "invoice_items"
+("invoice_id");
+CREATE INDEX ON "invoice_items"
+("treatment_id");
